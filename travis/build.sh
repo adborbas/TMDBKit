088fbb619904604e0f7764b4deece622565b8a3b
i303071 <adam.borbas@sap.com>
2018-02-26 01:25:02 +0100

    This should be good

--- travis/build.sh
+++ travis/build.sh
@@ -3,7 +3,7 @@
 # Variables
 SDK="iphonesimulator"
 workspace="TMDbKit.xcworkspace"
-destination='platform=iOS Simulator,name=iPhone 7,OS=11.2'
+destination="platform=iOS Simulator,name=iPhone 7,OS=11.2"
 
 # CLEAN
 xcodebuild -workspace $workspace -scheme BuildAll -sdk $SDK clean
@@ -11,11 +11,11 @@ xcodebuild -workspace $workspace -scheme BuildAll -sdk $SDK clean
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
   # Nightly BUILD
   # Integration Tests
-  xcodebuild -workspace $workspace -scheme IntegrationTests -sdk $SDK -destination $destination test
+  xcodebuild -workspace $workspace -scheme IntegrationTests -sdk $SDK -destination "$destination" test
 else
   # PullRequest BUILD
   # BUILD ALL
-  xcodebuild -workspace $workspace -scheme BuildAll -sdk $SDK -destination $destination
+  xcodebuild -workspace $workspace -scheme BuildAll -sdk $SDK -destination "$destination"
   # UNIT Tests
-  xcodebuild -workspace $workspace -scheme UnitTests -sdk $SDK -destination $destination test
+  xcodebuild -workspace $workspace -scheme UnitTests -sdk $SDK -destination "$destination" test
 fi
