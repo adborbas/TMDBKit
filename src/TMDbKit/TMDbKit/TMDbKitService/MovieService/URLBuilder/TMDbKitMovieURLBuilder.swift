7398b30307b9acc74e6701b608c5bcd8dff6cf5c
i303071 <adam.borbas@sap.com>
2018-03-04 00:54:47 +0100

    Reduce complexity

--- src/TMDbKit/TMDbKit/TMDbKitService/MovieService/URLBuilder/TMDbKitMovieURLBuilder.swift
+++ src/TMDbKit/TMDbKit/TMDbKitService/MovieService/URLBuilder/TMDbKitMovieURLBuilder.swift
@@ -33,7 +33,7 @@ class TMDbKitMovieURLBuilder: TMDbURLBuilder {
     
     func movieCreditsURL(for movieId: Int) -> URL {
         var components = self.baseURLComponents()
-        let movieCreditsPath = self.movieDetailPath(with: movieId).appending("/\(TMDbAPI.Movie.credits)")
+        let movieCreditsPath = self.movieDetailPath(with: movieId, method: TMDbAPI.Movie.credits)
         components.path.append(movieCreditsPath)
         
         return components.url!
@@ -41,13 +41,18 @@ class TMDbKitMovieURLBuilder: TMDbURLBuilder {
     
     func movieAlternativeTitles(for movieId: Int) -> URL {
         var components = self.baseURLComponents()
-        let movieCreditsPath = self.movieDetailPath(with: movieId).appending("/\(TMDbAPI.Movie.alternativeTitles)")
+        let movieCreditsPath = self.movieDetailPath(with: movieId, method: TMDbAPI.Movie.alternativeTitles)
         components.path.append(movieCreditsPath)
         
         return components.url!
     }
     
-    private func movieDetailPath(with movieId: Int) -> String {
-        return URL(string: "/")!.appendingPathComponent(TMDbAPI.Movie.path).appendingPathComponent("\(movieId)").absoluteString
+    private func movieDetailPath(with movieId: Int, method: String? = nil) -> String {
+        var url = URL(string: "/")!.appendingPathComponent(TMDbAPI.Movie.path).appendingPathComponent("\(movieId)")
+        if let method = method {
+            url.appendPathComponent(method)
+        }
+        
+        return url.absoluteString
     }
 }
