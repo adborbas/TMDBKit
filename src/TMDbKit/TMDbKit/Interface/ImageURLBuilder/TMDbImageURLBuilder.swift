a437f5e58eb519ef424c1e145aec60ec2b41f1e9
Adam Borbas <adborbas@gmail.com>
2018-03-16 14:28:29 +0100

    String enum values can be omitted

--- src/TMDbKit/TMDbKit/Interface/ImageURLBuilder/TMDbImageURLBuilder.swift
+++ src/TMDbKit/TMDbKit/Interface/ImageURLBuilder/TMDbImageURLBuilder.swift
@@ -28,20 +28,20 @@ public class TMDdImageURLBuilder {
     
     public enum Size {
         public enum Backdrop: String {
-            case w300 = "w300"
-            case w780 = "w780"
-            case w1280 = "w1280"
-            case original = "original"
+            case w300
+            case w780
+            case w1280
+            case original
         }
         
         public enum Poster: String {
-            case w92 = "w92"
-            case w154 = "w154"
-            case w185 = "w185"
-            case w342 = "w342"
-            case w500 = "w500"
-            case w780 = "w780"
-            case original = "original"
+            case w92
+            case w154
+            case w185
+            case w342
+            case w500
+            case w780
+            case original
         }
     }
     
